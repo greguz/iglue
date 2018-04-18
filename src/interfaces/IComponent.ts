79aa03d6d736b3031a218417bcc8112b49f13518
greguz <gregolettogiacomo@gmail.com>
2018-04-18 16:26:49 +0200

    Change component interface

--- src/interfaces/IComponent.ts
+++ src/interfaces/IComponent.ts
@@ -3,45 +3,51 @@ import { View } from "../View";
 export interface IComponent<T = any> {
 
   /**
-   * The component HTML template
+   * Returns the component HTML template
    */
 
-  template: string;
+  template: (this: T) => string;
 
   /**
    * Component loaded, data-binding and DOM not initialized
    */
 
-  create?(this: T): void;
+  create?: (this: T) => void;
 
   /**
    * DOM initialized, data-binding is not running
    */
 
-  attach?(this: T, el: HTMLElement): void;
+  attach?: (this: T, el: HTMLElement) => void;
 
   /**
    * Both DOM and data-binding are initialized
    */
 
-  bind?(this: T, view: View): void;
+  bind?: (this: T, view: View) => void;
 
   /**
    * The data-binding and the DOM are still running
    */
 
-  unbind?(this: T, view: View): void;
+  unbind?: (this: T, view: View) => void;
 
   /**
    * The data-binding is stopped, the DOM is still untached
    */
 
-  detach?(this: T, el: HTMLElement): void;
+  detach?: (this: T, el: HTMLElement) => void;
 
   /**
    * Both data-biding and DOM are dead
    */
 
-  destroy?(this: T): void;
+  destroy?: (this: T) => void;
+
+  /**
+   * Any other component method
+   */
+
+  [key: string]: (this: T, ...args: any[]) => any;
 
 }
