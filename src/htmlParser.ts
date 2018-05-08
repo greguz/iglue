b3624c6460e3653011bd92930f179080c0af3787
greguz <gregolettogiacomo@gmail.com>
2018-05-08 16:05:19 +0200

    Fix TS error

--- src/htmlParser.ts
+++ src/htmlParser.ts
@@ -214,7 +214,7 @@ export function parseHTML(html: string, options: IParseHTMLOptions): void {
 
     if (html === last) {
       options.chars && options.chars(html);
-      if (process.env.NODE_ENV !== 'production' && !stack.length && options.warn) {
+      if (/* process.env.NODE_ENV !== 'production' && */ !stack.length && options.warn) {
         options.warn(`Mal-formatted tag at end of template: "${html}"`);
       }
       break;
@@ -321,7 +321,7 @@ export function parseHTML(html: string, options: IParseHTMLOptions): void {
     if (pos >= 0) {
       // Close all the open elements, up the stack
       for (let i = stack.length - 1; i >= pos; i--) {
-        if (process.env.NODE_ENV !== 'production' &&
+        if (/* process.env.NODE_ENV !== 'production' && */
           (i > pos || !tagName) &&
           options.warn
         ) {
