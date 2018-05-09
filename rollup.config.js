829e31c2a78f735ff32e5250391abd735e9da145
greguz <gregolettogiacomo@gmail.com>
2018-05-09 17:25:26 +0200

    Add rollup builds

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import tsPlugin from 'rollup-plugin-typescript';
+import tsCompiler from 'typescript';
+
+export default {
+  input: 'src/index.ts',
+  plugins: [
+    tsPlugin({
+      typescript: tsCompiler
+    })
+  ]
+}
