42106b364aba1e8548d86e0084101282758ef8f4
greguz <gregolettogiacomo@gmail.com>
2018-05-18 19:53:44 +0200

    Fix TodoMVC example

--- examples/todomvc/app.js
+++ examples/todomvc/app.js
@@ -1,53 +1,72 @@
-iglue.formatters.remaining = function (todos) {
-  return todos.filter((todo) => todo.completed).length;
-}
+(function(window, iglue) {
 
-iglue.formatters.filterBy = function (todos, visibility) {
-  return todos.filter((todo) => {
-    if (visibility === 'active') {
+  iglue.formatters.remaining = function (todos) {
+    return todos.filter(function(todo) {
       return todo.completed !== true;
-    } else if (visibility === 'completed') {
-      return todo.completed === true;
-    } else {
-      return true;
-    }
-  });
-}
-
-const el = document.body;
-
-const data = {
-
-  visibility: 'all',
-
-  todos: [{
-    title: 'Take the cake',
-    completed: true
-  }, {
-    title: 'Kill Glados',
-    completed: false
-  }],
-
-  allDone(event, input) {
-    for (const todo of this.todos) {
-      todo.completed = input.checked;
-    }
-  },
-
-  removeCompleted() {
-    this.todos = this.todos.filter((todo) => {
-      return todo.completed !== true;
-    });
-  },
-
-  removeTodo(todo) {
-    this.todos = this.todos.filter((current) => current !== todo);
-  },
-
-  setVisibility(visibility) {
-    this.visibility = visibility;
+    }).length;
   }
 
-};
+  iglue.formatters.filterBy = function (todos, visibility) {
+    return todos.filter(function (todo) {
+      if (visibility === "active") {
+        return todo.completed !== true;
+      } else if (visibility === "completed") {
+        return todo.completed === true;
+      } else {
+        return true;
+      }
+    });
+  }
 
-window.view = iglue.bind(el, data);
+  window.data = {
+
+    newTodo: "",
+
+    visibility: "all",
+
+    todos: [{
+      title: "Take the cake",
+      completed: true
+    }, {
+      title: "Kill Glados",
+      completed: false
+    }],
+
+    allDone: function (event, input) {
+      this.todos = this.todos.map(function (todo) {
+        todo.completed = input.checked;
+        return todo;
+      });
+    },
+
+    removeCompleted: function () {
+      this.todos = this.todos.filter(function (todo) {
+        return todo.completed !== true;
+      });
+    },
+
+    removeTodo: function (todo) {
+      this.todos = this.todos.filter(function (current) {
+        return current !== todo;
+      });
+    },
+
+    setVisibility: function (visibility) {
+      this.visibility = visibility;
+    },
+
+    onNewTodoKeyUp: function (event) {
+      if (event.keyCode === 13) { // ENTER
+        this.todos.unshift({ title: this.newTodo });
+        this.newTodo = "";
+      }
+    }
+
+  };
+
+  window.view = iglue.bind(document.body, window.data);
+
+  // TODO editing
+  // TODO fix "items left"
+
+})(window, iglue);
