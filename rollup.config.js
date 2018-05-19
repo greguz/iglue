196758a297a9b94f7f493c35809657ae3228f534
greguz <gregolettogiacomo@gmail.com>
2018-05-19 11:19:23 +0200

    Add clean step before publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import tsPlugin from 'rollup-plugin-typescript';
-import tsCompiler from 'typescript';
+import tsPlugin from "rollup-plugin-typescript";
+import tsCompiler from "typescript";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   plugins: [
     tsPlugin({
       typescript: tsCompiler
