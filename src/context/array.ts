5687a4bf41341acf2a61e449c5729a945bd12394
greguz <gregolettogiacomo@gmail.com>
2019-01-13 10:09:33 +0100

    Improve array observing code

--- src/context/array.ts
+++ src/context/array.ts
@@ -1,10 +1,14 @@
 import { isArray, remove } from "../utils";
 
 /**
+ * Notifier function to call on array change
+ */
+export type ArrayNotifier = () => void;
+
+/**
  * Array methods to patch
  */
-
-const METHODS: string[] = [
+const METHODS = [
   "push",
   "pop",
   "shift",
@@ -15,121 +19,84 @@ const METHODS: string[] = [
 ];
 
 /**
- * Property where the notifiers are stored into the array
+ * Read current array notifiers
  */
-
-const STORE = "_oa_";
-
-/**
- * Represents an observed array
- */
-
-interface ObservedArray extends Array<any> {
-  [STORE]: ArrayNotifier[];
-}
-
-/**
- * Notifier function to call on array change
- */
-
-export type ArrayNotifier = () => void;
-
-/**
- * Apply observe middleware to the array instance
- */
-
-function applyMiddleware(arr: ObservedArray): void {
-  // ensure array data store
-  if (!arr.hasOwnProperty(STORE)) {
-    Object.defineProperty(arr, STORE, {
-      // not configurable, prevent double definition
-      // not enumerable, prevent Object.assign cloning
-      // not writable, prevent value assignation/override
-      value: []
-    });
-  }
-
-  // wrap all array methods that changes the array structure
-  for (const method of METHODS) {
-    // override the default method
-    Object.defineProperty(arr, method, {
-      configurable: true,
-      // not enumerable, prevent Object.assign cloning
-      writable: true,
-      value: function middleware(this: ObservedArray, ...args: any[]): any {
-        // call the original method and get the result
-        const result: any = (Array as any).prototype[method].apply(this, args);
-        // trigger all notifiers
-        for (const notifier of this[STORE]) {
-          notifier();
-        }
-        // return the result
-        return result;
-      }
-    });
-  }
-}
-
-/**
- * Remove the observe middleware
- */
-
-function removeMiddleware(arr: ObservedArray): void {
-  for (const method of METHODS) {
-    // restore the original method
-    Object.defineProperty(arr, method, {
-      configurable: true,
-      // not enumerable, prevent Object.assign cloning
-      writable: true,
-      value: (Array as any).prototype[method]
-    });
-  }
-}
-
-/**
- * Returns true when the array is observed
- */
-
-export function isObservedArray(arr: any): boolean {
-  if (isArray(arr) && arr.hasOwnProperty(STORE)) {
-    return arr[STORE].length > 0;
-  }
-  return false;
-}
-
-/**
- * Observe the array
- */
-
-export function observeArray(arr: any, notifier: ArrayNotifier): void {
+function read(arr: any, property: string = "_oa_"): ArrayNotifier[] {
   if (!isArray(arr)) {
-    throw new Error("Unexpected array to observe");
+    throw new Error("Argument is not an array");
   }
-  if (!isObservedArray(arr)) {
-    applyMiddleware(arr);
+  if (!arr.hasOwnProperty(property)) {
+    Object.defineProperty(arr, property, { value: [] });
   }
-  arr[STORE].push(notifier);
+  return arr[property];
+}
+
+/**
+ * Apply notifiers triggering to a function
+ */
+function wrap(fn: (...args: any[]) => any, notifiers: ArrayNotifier[]) {
+  return function wrapped(...args: any[]): any {
+    // call the original method and get the result
+    const result = fn.apply(this, args);
+
+    // trigger all notifiers
+    for (const notifier of notifiers) {
+      notifier();
+    }
+
+    // return the result
+    return result;
+  };
+}
+
+/**
+ * Observe array changes
+ */
+export function observeArray(arr: any, notifier: ArrayNotifier): void {
+  // Read current notifiers
+  const notifiers = read(arr);
+
+  // Apply custom middleware if necessary
+  if (notifiers.length === 0) {
+    for (const method of METHODS) {
+      Object.defineProperty(arr, method, {
+        configurable: true,
+        value: wrap((Array.prototype as any)[method], notifiers)
+      });
+    }
+  }
+
+  // Save the target notifier
+  notifiers.push(notifier);
 }
 
 /**
  * Remove the observe notifier, returns true when the notifier is removed
  */
-
 export function unobserveArray(arr: any, notifier: ArrayNotifier): boolean {
-  if (isObservedArray(arr)) {
-    // get the notifiers array
-    const notifiers: ArrayNotifier[] = arr[STORE];
+  // Read current notifiers
+  const notifiers = read(arr);
 
-    // remove the targeted notifier
-    const removed: boolean = remove(notifiers, notifier);
+  // Try to remove the target notifier
+  const removed = remove(notifiers, notifier);
 
-    // remove middleware if necessary
-    if (notifiers.length === 0) {
-      removeMiddleware(arr);
+  // Remove custom middleware if necessary
+  if (notifiers.length === 0) {
+    for (const method of METHODS) {
+      Object.defineProperty(arr, method, {
+        configurable: true,
+        value: (Array.prototype as any)[method]
+      });
     }
-
-    // all done
-    return removed;
   }
-  return false;
+
+  // Return removal result
+  return removed;
+}
+
+/**
+ * Returns true when the argument is an observed array
+ */
+export function isArrayObserved(arr: any): boolean {
+  return read(arr).length > 0;
 }
