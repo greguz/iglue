cb22ad9a769afba266f5a4ecb53cd29c76b3e87b
greguz <gregolettogiacomo@gmail.com>
2019-01-14 15:31:19 +0100

    Improve text directive

--- src/directives/text.ts
+++ src/directives/text.ts
@@ -1,12 +1,12 @@
 import { Directive } from "../interfaces/Directive";
 
+import { noop } from "../utils";
+
 export function buildTextDirective(node: Text): Directive {
   return {
     refresh(value: any): void {
       node.data = value === undefined || value === null ? "" : value.toString();
     },
-    unbind(): void {
-      // keep the last value
-    }
+    unbind: noop
   };
 }
