9facc3e823fc8e0971e7afef156ca7c99ca1f0da
greguz <gregolettogiacomo@gmail.com>
2019-01-15 12:05:24 +0100

    Add more utils

--- src/utils.ts
+++ src/utils.ts
@@ -102,6 +102,20 @@ export function isFunction(value: any): value is Function {
 }
 
 /**
+ * Returns true if the argument is a string
+ */
+export function isString(value: any): value is string {
+  return typeof value === "string";
+}
+
+/**
+ * Returns true if the argument is a finite number
+ */
+export function isNumber(value: any): value is number {
+  return typeof value === "number" && !isNaN(value) && isFinite(value);
+}
+
+/**
  * Like Array.prototype.includes
  */
 export function includes<T>(arr: T[], target: T): boolean {
