d1fb068070fa62d83fbf4bc5440f3b28f671888e
greguz <gregolettogiacomo@gmail.com>
2019-01-22 13:29:15 +0100

    Fix text directive

--- src/directives/text.ts
+++ src/directives/text.ts
@@ -3,17 +3,20 @@ import { Directive } from "../interfaces/Directive";
 
 import { parseAttributeValue } from "../parse/attribute";
 
-import { noop } from "../utils";
-
 export function buildTextDirective(this: App, node: Text): Directive {
+  // Save the original node's content
+  const content = node.data;
+
   return {
     // Expression info
-    ...parseAttributeValue(node.data),
+    ...parseAttributeValue(content),
     // Refresh node content
     update(this: App, value: any) {
       node.data = value === undefined || value === null ? "" : value.toString();
     },
-    // Nothing to do on unbind
-    unbind: noop
+    // Restore original content
+    unbind(this: App) {
+      node.data = content;
+    }
   };
 }
